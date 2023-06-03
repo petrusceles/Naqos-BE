64278610787dc2539a54b163daac66e9f3409aa8
petrusceles <petrusprihantoro@gmail.com>
2023-06-03 17:24:51 +0700

    feature kost type done

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
-mongoose.connect(
-  `mongodb+srv://naqosdb:${process.env.DB_PASSWORD}@naqosdb.ppdjsum.mongodb.net/?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
