5ed66200deab42b15132e7ce96bfee0c22e615cf
petrusceles <petrusprihantoro@gmail.com>
2023-10-10 01:31:55 +0700

    save uninitialized

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
       secure: true,
